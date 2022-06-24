f9389b978604e8538d85c885ad3f03d620e56fb7
jonginkoo <jiku90@ddingdong.net>
2022-06-25 07:35:37 +0900

    feat: update lucky draw feature it easier for users to classify grade of card

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -6,21 +6,73 @@ import Tippy from '@tippyjs/react';
 export enum CardNameEnum {
   NIDALEE = 'nidalee',
   NIDALEE_SONG_OF_THE_SEE = 'nidalee.song-of-the-see',
+  XAYAH = 'xayah',
+  XAYAH_ARCANA = 'xayah.arcana',
+  THRESH = 'thresh',
+  TAHMKENCH = 'tahmkench',
+  TAHMKENCH_ARCANA = 'tahmkench.arcana',
 }
-type CardNameKind = CardNameEnum;
+export type CardNameKind = typeof CardNameEnum;
 const StyledCard = styled.span`
   .card-box {
     border: 4px solid transparent;
     border-radius: 5px;
-    background-image: linear-gradient(#444444, #444444),
-      linear-gradient(to right, #fbfcb9be, #ffcdf3aa, #65d3ffaa);
     background-origin: border-box;
     background-clip: content-box, border-box;
+
+    &.tier-rare {
+      background-image: linear-gradient(#502b00, #ff4e00),
+        linear-gradient(to right, #7e3a00, #7d3a00, #a35c00);
+    }
+
+    &.tier-sr {
+      background-image: linear-gradient(to right, #89a67b, #351717),
+        linear-gradient(
+          324deg,
+          #f1c40f,
+          #f1c40f,
+          #f1c40f,
+          rgb(251 255 70),
+          #f1c40f,
+          #ffda00
+        );
+      //box-shadow: 0 0 2px 0px #65d3ffaa, 0 0 2px 0px #ffcdf3aa,
+      //  0 0 2px 0px #fbfcb9be;
+    }
+
+    &.tier-ssr {
+      background-image: linear-gradient(#ffffff, #ffffff),
+        linear-gradient(
+          137deg,
+          #ff8b88,
+          #2a51ff,
+          #ffa604aa,
+          #71ff00aa,
+          #65ffe5aa,
+          #1cbeffaa
+        );
+      box-shadow: 0 0 4px 0px #ff8b88, 0 0 4px 0px #ffa604aa,
+        0 0 4px 0px #a6ff5faa, 0 0 4px 0px #65ffe5aa, 0 0 4px 0px #1cbeffaa;
+    }
   }
 `;
-export default function Card({ name }: { name: CardNameKind }) {
+
+export interface ICard {
+  name: CardNameKind | string;
+  tier: string;
+}
+
+interface CardProps
+  extends React.DetailedHTMLProps<
+    React.HTMLAttributes<HTMLElement>,
+    HTMLElement
+  > {
+  card: ICard;
+}
+export default function Card({ card, style }: CardProps) {
+  const { name, tier } = card;
   return (
-    <StyledCard>
+    <StyledCard style={style}>
       <Tippy
         className="no-padding no-border-radius"
         content={<img height={300} src={`/resources/card/${name}.png`} />}
@@ -28,7 +80,7 @@ export default function Card({ name }: { name: CardNameKind }) {
         arrow={false}
       >
         <div
-          className="card-box"
+          className={`card-box tier-${tier}`}
           style={{
             cursor: 'pointer',
             position: 'relative',
--- src/pages/lucky-draw/index.tsx
+++ src/pages/lucky-draw/index.tsx
@@ -1,7 +1,8 @@
-import React from 'react';
+import React, { BaseSyntheticEvent, useEffect, useRef, useState } from 'react';
 import styled from 'styled-components';
+import _ from 'lodash';
 import { LAYOUT_MAX_WIDTH, LAYOUT_MIN_WIDTH } from '../../config/variables';
-import Card, { CardNameEnum } from '../../components/Card';
+import Card, { CardNameEnum, CardNameKind } from '../../components/Card';
 
 const StyledLuckyDraw = styled.div`
   min-width: ${LAYOUT_MIN_WIDTH}px;
@@ -17,19 +18,21 @@ const StyledLuckyDraw = styled.div`
     bottom: 0;
     left: 0;
     right: 0;
-    margin: 10%;
-    max-height: 100%;
-    height: 100vh;
+    margin: auto;
+    width: 80vw;
+    height: 80vh;
+    //max-height: 100%;
     box-sizing: border-box;
     overflow: hidden;
-    display: flex;
-    align-items: center;
-    justify-content: center;
     background: #008dff29;
+    video {
+      max-height: inherit !important;
+    }
   }
 
   .video-wrapper {
-    width: 300px;
+    width: 700px;
+    position: relative;
   }
   .shadow {
     opacity: 0.9;
@@ -46,9 +49,68 @@ const StyledLuckyDraw = styled.div`
 `;
 
 function LuckyDraw() {
+  const videoElementRef = useRef<HTMLVideoElement>(null);
+  const [isEndedVideo, setIsEndedVideo] = useState<boolean>(false);
+  const [isWaitingDraw, setIsWaitingDraw] = useState<boolean>(false);
+  const [dropList, setDropList] = useState<any[]>([]);
+
+  const onClickDrawButton = () => {
+    videoElementRef?.current?.play();
+    setIsWaitingDraw(true);
+  };
+
+  const getRandomCard = () => {
+    const cardNames = Object.values(CardNameEnum);
+    const tiers = ['rare', 'sr', 'ssr'];
+    const [randomTier] = _.shuffle(tiers);
+    const [randomCardName] = _.shuffle(cardNames);
+    return {
+      name: randomCardName,
+      tier: randomTier,
+    };
+  };
+  const onEndedDrawVideo = () => {
+    getRandomCard();
+    setIsEndedVideo(true);
+    setIsWaitingDraw(false);
+    setDropList(origin => {
+      return [...origin, ...[getRandomCard()]];
+    });
+    console.log(dropList);
+  };
+
+  const onTimeUpdate = (event: BaseSyntheticEvent) => {
+    const { target } = event;
+    const { currentTime } = target;
+    const DRAW_VIDEO_END_TIMESTAMP = 6.18;
+    if (currentTime >= DRAW_VIDEO_END_TIMESTAMP) {
+      setIsEndedVideo(true);
+    } else {
+      setIsEndedVideo(false);
+    }
+  };
+
+  const onClickSkipButton = () => {
+    if (!videoElementRef.current) return;
+    videoElementRef.current.currentTime = 999;
+  };
+  useEffect(() => {
+    if (!videoElementRef.current) return;
+    const { current } = videoElementRef;
+    current.volume = 0.5;
+  }, [videoElementRef]);
+
+  useEffect(() => {
+    if (!isEndedVideo) return;
+
+    console.log(isWaitingDraw, 'video end');
+  }, [isEndedVideo]);
   return (
     <StyledLuckyDraw>
       <div
+        tabIndex={0}
+        role="button"
+        onClick={onClickDrawButton}
         className="shadow noselect"
         style={{
           cursor: 'pointer',
@@ -64,14 +126,58 @@ function LuckyDraw() {
         <div>패시브</div>
         <div>카드뽑기</div>
       </div>
-      <div>
-        {/* <Card name={CardNameEnum.NIDALEE} /> */}
-        {/* <Card name={CardNameEnum.NIDALEE_SONG_OF_THE_SEE} /> */}
-      </div>
-      <div className="video-wrapper">
-        <video autoPlay style={{ maxWidth: '100%', maxHeight: '100%' }}>
+      <div className={`video-wrapper ${isWaitingDraw ? '' : 'hide'}`}>
+        <video
+          onTimeUpdate={onTimeUpdate}
+          onEnded={onEndedDrawVideo}
+          ref={videoElementRef}
+          style={{ maxWidth: '100%', maxHeight: '100%' }}
+        >
           <source src="/resources/video/gatcha.mp4" type="video/mp4" />
         </video>
+        <div
+          tabIndex={0}
+          role="button"
+          onClick={onClickSkipButton}
+          className={isEndedVideo ? 'hide' : ''}
+          style={{
+            position: 'absolute',
+            right: 0,
+            bottom: 0,
+            margin: '10px 5px',
+          }}
+        >
+          <div
+            className="shadow noselect"
+            style={{
+              position: 'relative',
+              minWidth: 80,
+              padding: '1px 0',
+              textAlign: 'center',
+              cursor: 'pointer',
+              border: 'solid 1px',
+              borderRadius: 5,
+              background: 'rgba(255,255,255,0.9)',
+            }}
+          >
+            Skip
+          </div>
+        </div>
+      </div>
+
+      <div>
+        <div>드랍 목록</div>
+        <div style={{ display: 'inline-flex', flexFlow: 'wrap' }}>
+          {dropList.map((card, index) => {
+            return (
+              <Card
+                style={{ marginRight: 5, marginBottom: 5 }}
+                key={`dropped-card-${index + 0}-${card.name}`}
+                card={card}
+              />
+            );
+          })}
+        </div>
       </div>
     </StyledLuckyDraw>
   );
